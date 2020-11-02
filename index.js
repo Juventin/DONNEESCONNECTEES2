c7e87ea4a11d7f1db18593a420389e9e74282c19
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-02 16:36:23 +0100

    cors

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const port = process.env.PORT || 3000 ;
 
 var fetch = require('node-fetch');
 var https = require('https');
+var fs = require("fs");
+
 const googleTrends = require('google-trends-api');
 
+var cors = require('cors');
+var corsOptions = {
+    origin: [
+        'https://netflixbutnochill.herokuapp.com/', 
+        'https://juventin.github.io/DONNEESCONNECTEES2/'
+    ],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 
 
 //serves static files
 app.use(express.static('docs'));
 
-
-
-
-app.get("/", function(req, res){
-    res.send("helloWorld !");
-})
 
 app.get("/:name", function(req, res){
     res.send("hello : " + req.params.name );
@@ -40,36 +44,16 @@ app.get("/trends/netflix", function(req, res){
     });
 })
 
-app.get("/cocktail/margarita", function(req, res){
-    console.log('ok')
-    let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita" ;
-    fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        res.send(json);
-    });
-})
+// app.get("/cocktail/margarita", function(req, res){
+//     console.log('ok')
+//     let url = "https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2020T2.xls" ;
+//     console.log('on y va')
+//     fetch(url)
+//     .then(res => res.text())
+//     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+//     .then(data => console.log(data))
+// })
 
-app.get("/requestair/shangai", function(req, res){
-    let url = "https://api.waqi.info/feed/shanghai/?token=demo" ;
-    https.get(url, (resp) => {
-        let data = '';
-
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-         });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log("requestair", JSON.parse(data));
-            res.send("data requested look your console");
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.send("nope request didnt work");
-    });
-})
 
 app.listen(port, function () {
     console.log('Serveur listening on port ' + port);
