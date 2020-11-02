cc0f1d75ff04094786bbabd757c565f40ae7c877
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2020-11-02 18:48:11 +0100

    cleaning index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 
 var fetch = require('node-fetch');
 var https = require('https');
@@ -14,46 +14,58 @@ const googleTrends = require('google-trends-api');
 var cors = require('cors');
 var corsOptions = {
     origin: [
-        'https://netflixbutnochill.herokuapp.com/', 
+        'https://netflixbutnochill.herokuapp.com/',
         'https://juventin.github.io/DONNEESCONNECTEES2/'
     ],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 
+function getTrendsPerRegion() {
+    //serves static files
+    app.use(express.static('docs'));
 
-//serves static files
-app.use(express.static('docs'));
 
-
-app.get("/:name", function(req, res){
-    res.send("hello : " + req.params.name );
-})
-
-app.get("/trends/netflix", function(req, res){
-    googleTrends.interestByRegion({
-        keyword: 'netflix',
-        geo: "FR",
-        resolution: "REGION"
+    app.get("/:name", function (req, res) {
+        res.send("hello : " + req.params.name);
     })
-    .then(function(results){
-        res.send(results);
+
+    app.get("/trends/netflix", function (req, res) {
+        googleTrends.interestByRegion({
+                keyword: 'netflix',
+                geo: "FR",
+                resolution: "REGION"
+            })
+            .then(function (results) {
+                res.send(results);
+            })
+            .catch(function (err) {
+                res.send('Oh no there was an error', err);
+            });
     })
-    .catch(function(err){
-        res.send('Oh no there was an error', err);
-    });
-})
+}
 
-// app.get("/cocktail/margarita", function(req, res){
-//     console.log('ok')
-//     let url = "https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2020T2.xls" ;
-//     console.log('on y va')
-//     fetch(url)
-//     .then(res => res.text())
-//     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-//     .then(data => console.log(data))
-// })
+function getInformationsAboutTheCSP() {
+    app.get("/cocktail/margarita", function (req, res) {
+        console.log('ok')
+        let url = "https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2020T2.xls";
+        console.log('on y va')
+        fetch(url)
+            .then(res => res.text())
+            .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+            .then(data => console.log(data))
+    })
+}
 
+function getInformationsAboutTheMovie() {
+    var input = document.getElementById("name");
+    var result;
+    var url = "https://api.betaseries.com/movies/search?title=" + input.value;
+    fetch(url)
+        .then(res => res.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(data => console.log(data))
+}
 
 app.listen(port, function () {
     console.log('Serveur listening on port ' + port);
