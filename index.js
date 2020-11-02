0ef249d41bb94ad0e1f114077367bcf83d8809c8
GaelleNovales <47526451+GaelleNovales@users.noreply.github.com>
2020-11-02 17:15:48 +0100

    allocine
    renvois un film

--- index.js
+++ index.js
@@ -44,6 +44,24 @@ app.get("/trends/netflix", function(req, res){
     });
 })
 
+
+// Recherche de tous les films "spiderman"
+allocine.api('search', {q: 'spiderman', filter: 'movie'}, function(error, results) {
+    if(error) { console.log('Error : '+ error); return; }
+    
+    console.log('Voici les données retournées par l\'API Allociné:');
+    console.log(results);
+});
+
+// Informations sur un film particulier
+allocine.api('movie', {code: '143067'}, function(error, result) {
+    if(error) { console.log('Error : '+ error); return; }
+
+    console.log('Voici les données retournées par l\'API Allociné:');
+    console.log(result);
+});
+
+
 // app.get("/cocktail/margarita", function(req, res){
 //     console.log('ok')
 //     let url = "https://www.insee.fr/fr/statistiques/fichier/2012804/sl_etc_2020T2.xls" ;
