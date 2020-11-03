75d639595ac37bb9889a8f2be5cb0031b56a22f9
GaelleNovales <47526451+GaelleNovales@users.noreply.github.com>
2020-11-03 10:33:33 +0100

    ajout deux api test allociné

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var https = require('https');
 var fs = require("fs");
 
 const googleTrends = require('google-trends-api');
+var allocine = require('allocine-api');
 
 app.get("/", function(req, res){
     res.send("coucou");
@@ -25,6 +26,25 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+//Gaelle test
+
+// Recherche de tous les films "spiderman"
+allocine.api('search', {q: 'spiderman', filter: 'movie'}, function(error, results) {
+    if(error) { console.log('Error : '+ error); return; }
+    
+    console.log('Voici les données retournées par l\'API Allociné:');
+    console.log(results);
+});
+
+// Informations sur un film particulier
+allocine.api('movie', {code: '143067'}, function(error, result) {
+    if(error) { console.log('Error : '+ error); return; }
+
+    console.log('Voici les données retournées par l\'API Allociné:');
+    console.log(result);
+});
+
+//fin test gaelle
 
 function getTrendsPerRegion() {
     //serves static files
