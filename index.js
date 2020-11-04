621d19fac85658474419a9cfb401641694d13a1c
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2020-11-04 10:19:41 +0100

    fixup! fixup! fixup! adding latitude and longitude in json

--- index.js
+++ index.js
@@ -70,81 +70,83 @@ function mergeDataNoJointure(arr1, arr2) {
 
 app.get("/trends", cors(corsOptions), async function (req, res) {
 
-    // On récupère régions
-    var regions = getRegions();
+            // On récupère régions
+            var regions = getRegions();
 
-    // On récupère le film demandé
-    var movie = req.param("title");
-    console.log(movie)
+            // On récupère le film demandé
+            var movie = req.param("title");
+            console.log(movie)
 
-    // On récupère les données du film demandé
-    var films;
-    var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
-    await fetch(url)
-        .then(res => res.json())
-        .then(json => {
-            films = json;
-        })
+            // On récupère les données du film demandé
+            var films;
+            var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
+            await fetch(url)
+                .then(res => res.json())
+                .then(json => {
+                    films = json;
+                })
 
-    // On récupère les données météo
-    var result = '';
-    var meteo = new Array();
-    for (var i = 0; i < regions.length; i++) {
-        var lat = regions[i].lat;
-        var lng = regions[i].lng;
-        var key = regions[i].Code;
-        await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
-            .then(function (response) {
-                response.json()
-                    .then(function (data) {
-                        result = data;
-                        result.code = key;
-                        meteo.push(result);
+
+            // On récupère les données météo
+            var result = '';
+            var meteo = new Array();
+            for (var i = 0; i < regions.length; i++) {
+                var lat = regions[i].lat;
+                var lng = regions[i].lng;
+                var key = regions[i].Code;
+                await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
+                    .then(function (response) {
+                        response.json()
+                            .then(function (data) {
+                                result = data;
+                                result.code = key;
+                                meteo.push(result);
+                            })
                     })
-            })
-    }
-
-    var yesterday = new Date(new Date().setDate(new Date().getDate()-3))
-
-    // On récupère les données google trends
-    // Et on fait notre fusion de données avec ces données
-    googleTrends.interestByRegion({
-        keyword: movie,
-        geo: "FR",
-        resolution: "REGION",
-        startTime: yesterday
-    })
-    .then(res => JSON.parse(res))
-    .then(json => {
-        //console.log(result);
-
-        // Ok pour trends
-        var trends = json['default']['geoMapData']
-
-        // On jointe les deux sur Libelle == geoName
-        var merged = mergeData(regions, 'Libelle', trends, 'geoName')
-
-        // On joint merged avec film
-        var merged2 = mergeDataNoJointure(merged, films);
-
-        // On jointe les deux merged sur code == Code
-        var merged3 = mergeData(merged2, "Code", meteo, "code");
-
-        // On le renvoie
-        res.format({
-            /*'text/html': function () {
-                console.log(merged3)
-                res.send("data fetched look your console");
-            },*/
-            'application/json': function () {
-                res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-                res.set('Content-Type', 'application/json');
-                res.json(merged3);
             }
+        }
+
+        var yesterday = new Date(new Date().setDate(new Date().getDate() - 3))
+
+        // On récupère les données google trends
+        // Et on fait notre fusion de données avec ces données
+        googleTrends.interestByRegion({
+            keyword: movie,
+            geo: "FR",
+            resolution: "REGION",
+            startTime: yesterday
         })
-})
+        .then(res => JSON.parse(res))
+        .then(json => {
+            //console.log(result);
+
+            // Ok pour trends
+            var trends = json['default']['geoMapData'];
+
+            // On jointe les deux sur Libelle == geoName
+            var merged = mergeData(regions, 'Libelle', trends, 'geoName');
+
+            // On joint merged avec film
+            var merged2 = mergeDataNoJointure(merged, films);
+
+            // On jointe les deux merged sur code == Code
+            var merged3 = mergeData(merged2, "Code", meteo, "code");
+
+            // On le renvoie
+            res.format({
+                /*'text/html': function () {
+                    console.log(merged3)
+                    res.send("data fetched look your console");
+                },*/
+                'application/json': function () {
+                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
+                    res.set('Content-Type', 'application/json');
+                    res.json(merged3);
+                }
+            })
+        })
 
 
-app.listen(port, function () {
-    console.log('Serveur listening on port ' + port);
-});
+        app.listen(port, function () {
+            console.log('Serveur listening on port ' + port);
+        });
