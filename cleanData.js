e77b0a5b504e6fd5de83178efc70a18bc8882b53
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-04 12:26:04 +0100

    clean data regions

--- cleanData.js
+++ cleanData.js
@@ -6,16 +6,16 @@ function cleanData(data, type) {
         for (let i = 0; i < data.length; i++) {
             let row = {}
             row.Code = data[i]['Code']
-            try { row.Date = data[i]['current_condition']['date'] } catch (e) { console.log('oui') }
-            try { row.Hour = data[i]['current_condition']['hour'] } catch (e) { console.log('oui') }
+            try { row.Date = data[i]['current_condition']['date'] } catch (e) { }
+            try { row.Hour = data[i]['current_condition']['hour'] } catch (e) { }
             row.Region = data[i]['Libelle']
-            try { row.Taux_chomage = data[i]['T2_2020'] } catch (e) { console.log('oui') }
-            try { row.Temperature = data[i]['current_condition']['tmp'] } catch (e) { console.log('oui') }
-            try { row.Wind_speed = data[i]['current_condition']['wind_spd'] } catch (e) { console.log('oui') }
-            try { row.Wind_direction = data[i]['current_condition']['wind_dir'] } catch (e) { console.log('oui') }
-            try { row.pressure = data[i]['current_condition']['pressure'] } catch (e) { console.log('oui') }
-            try { row.humidity = data[i]['current_condition']['humidity'] } catch (e) { console.log('oui') }
-            try { row.condition_climatique = data[i]['current_condition']['condition'] } catch (e) { console.log('oui') }
+            try { row.Taux_chomage = data[i]['T2_2020'] } catch (e) { }
+            try { row.Temperature = data[i]['current_condition']['tmp'] } catch (e) { }
+            try { row.Wind_speed = data[i]['current_condition']['wnd_spd'] } catch (e) { }
+            try { row.Wind_direction = data[i]['current_condition']['wnd_dir'] } catch (e) { }
+            try { row.pressure = data[i]['current_condition']['pressure'] } catch (e) { }
+            try { row.humidity = data[i]['current_condition']['humidity'] } catch (e) { }
+            try { row.condition_climatique = data[i]['current_condition']['condition'] } catch (e) { }
             data2.push(row);
         }
     }
