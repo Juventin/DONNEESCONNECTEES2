9d5ee85a5cda40933ea32f9be130084d0423609a
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-04 16:11:27 +0100

    data clean optimisé

--- cleanData.js
+++ cleanData.js
@@ -12,7 +12,7 @@ function cleanData(data, type) {
                 row.Date = data[i]['current_condition']['date']
                 row.Hour = data[i]['current_condition']['hour']
                 row.Taux_chomage = data[i]['T2_2020']
-                row.Meteo = []
+                row.Meteo = {}
                 row.Meteo.Temperature = data[i]['current_condition']['tmp']
                 row.Meteo.Wind_speed = data[i]['current_condition']['wnd_spd']
                 row.Meteo.Wind_direction = data[i]['current_condition']['wnd_dir']
@@ -41,12 +41,13 @@ function cleanData(data, type) {
     }
 
     if (type == 'trends') {
+        data2 = {}
         data2.Code_region = data[0][0]['Code']
         data2.Region = data[0][0]['Libelle']
         data2.Date = data[0][0]['Date']
         data2.Hour = data[0][0]['Hour']
         data2.Taux_chomage = data[0][0]['Taux_chomage']
-        data2.Meteo = []
+        data2.Meteo = {}
         data2.Meteo.Temperature = data[0][0]['Temperature']
         data2.Meteo.Wind_speed = data[0][0]['Wind_speed']
         data2.Meteo.Wind_direction = data[0][0]['Wind_direction']
@@ -68,9 +69,7 @@ function cleanData(data, type) {
             row.Original_language = data[i]['language']
             data2.Films.push(row);
         }
-        console.log(data2)
     }
-
     return data2
 }
 
--- index.js
+++ index.js
@@ -150,6 +150,7 @@ app.get("/trends/:movie/:region", cors(corsOptions), async function (req, res) {
             // On joint merged avec film
             var merged3 = mergeDataNoJointure(films.movies, merged2);
             merged3 = cleanData(merged3, 'trends')
+            console.log(merged3)
 
 
             /*// On jointe les deux sur Libelle == geoName
