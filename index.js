bde6809887e235ed4e77f1221c4148f5fab8a1fc
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2020-11-04 10:26:17 +0100

    rename json input

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.static('docs'));
 
 function getRegions() {
 
-    let filePath = path.join('files', 'chomage.json');
+    let filePath = path.join('files', 'regions.json');
 
     let rawdata = fs.readFileSync(filePath);
     let data = JSON.parse(rawdata);
@@ -70,46 +70,46 @@ function mergeDataNoJointure(arr1, arr2) {
 
 app.get("/trends", cors(corsOptions), async function (req, res) {
 
-        // On récupère régions
-        var regions = getRegions();
+    // On récupère régions
+    var regions = getRegions();
 
-        // On récupère le film demandé
-        var movie = req.param("title");
-        console.log(movie)
+    // On récupère le film demandé
+    var movie = req.param("title");
+    console.log(movie)
 
-        // On récupère les données du film demandé
-        var films;
-        var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
-        await fetch(url)
-            .then(res => res.json())
-            .then(json => {
-                films = json;
+    // On récupère les données du film demandé
+    var films;
+    var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
+    await fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            films = json;
+        })
+
+
+    // On récupère les données météo
+    var result = '';
+    var meteo = new Array();
+    for (var i = 0; i < regions.length; i++) {
+        var lat = regions[i].lat;
+        var lng = regions[i].lng;
+        var key = regions[i].Code;
+        await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        result = data;
+                        result.code = key;
+                        meteo.push(result);
+                    })
             })
+    }
 
+    var yesterday = new Date(new Date().setDate(new Date().getDate() - 3))
 
-        // On récupère les données météo
-        var result = '';
-        var meteo = new Array();
-        for (var i = 0; i < regions.length; i++) {
-            var lat = regions[i].lat;
-            var lng = regions[i].lng;
-            var key = regions[i].Code;
-            await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
-                .then(function (response) {
-                    response.json()
-                        .then(function (data) {
-                            result = data;
-                            result.code = key;
-                            meteo.push(result);
-                        })
-                })
-        }
-
-        var yesterday = new Date(new Date().setDate(new Date().getDate() - 3))
-
-        // On récupère les données google trends
-        // Et on fait notre fusion de données avec ces données
-        googleTrends.interestByRegion({
+    // On récupère les données google trends
+    // Et on fait notre fusion de données avec ces données
+    googleTrends.interestByRegion({
             keyword: movie,
             geo: "FR",
             resolution: "REGION",
