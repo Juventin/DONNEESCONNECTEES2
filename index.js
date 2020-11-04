552eede81b3dc973ae282540d283ba880aa6e8a7
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-04 10:22:56 +0100

    debug

--- index.js
+++ index.js
@@ -70,40 +70,39 @@ function mergeDataNoJointure(arr1, arr2) {
 
 app.get("/trends", cors(corsOptions), async function (req, res) {
 
-            // On récupère régions
-            var regions = getRegions();
+        // On récupère régions
+        var regions = getRegions();
 
-            // On récupère le film demandé
-            var movie = req.param("title");
-            console.log(movie)
+        // On récupère le film demandé
+        var movie = req.param("title");
+        console.log(movie)
 
-            // On récupère les données du film demandé
-            var films;
-            var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
-            await fetch(url)
-                .then(res => res.json())
-                .then(json => {
-                    films = json;
+        // On récupère les données du film demandé
+        var films;
+        var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
+        await fetch(url)
+            .then(res => res.json())
+            .then(json => {
+                films = json;
+            })
+
+
+        // On récupère les données météo
+        var result = '';
+        var meteo = new Array();
+        for (var i = 0; i < regions.length; i++) {
+            var lat = regions[i].lat;
+            var lng = regions[i].lng;
+            var key = regions[i].Code;
+            await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
+                .then(function (response) {
+                    response.json()
+                        .then(function (data) {
+                            result = data;
+                            result.code = key;
+                            meteo.push(result);
+                        })
                 })
-
-
-            // On récupère les données météo
-            var result = '';
-            var meteo = new Array();
-            for (var i = 0; i < regions.length; i++) {
-                var lat = regions[i].lat;
-                var lng = regions[i].lng;
-                var key = regions[i].Code;
-                await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng)
-                    .then(function (response) {
-                        response.json()
-                            .then(function (data) {
-                                result = data;
-                                result.code = key;
-                                meteo.push(result);
-                            })
-                    })
-            }
         }
 
         var yesterday = new Date(new Date().setDate(new Date().getDate() - 3))
@@ -146,7 +145,8 @@ app.get("/trends", cors(corsOptions), async function (req, res) {
             })
         })
 
+})
 
-        app.listen(port, function () {
-            console.log('Serveur listening on port ' + port);
-        });
+app.listen(port, function () {
+    console.log('Serveur listening on port ' + port);
+});
