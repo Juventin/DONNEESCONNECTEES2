a27915188c4fd4dec9b212baa4e9e7d9611f73d7
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-04 16:04:24 +0100

    clean data optimisé

--- cleanData.js
+++ cleanData.js
@@ -12,12 +12,13 @@ function cleanData(data, type) {
                 row.Date = data[i]['current_condition']['date']
                 row.Hour = data[i]['current_condition']['hour']
                 row.Taux_chomage = data[i]['T2_2020']
-                row.Temperature = data[i]['current_condition']['tmp']
-                row.Wind_speed = data[i]['current_condition']['wnd_spd']
-                row.Wind_direction = data[i]['current_condition']['wnd_dir']
-                row.pressure = data[i]['current_condition']['pressure']
-                row.humidity = data[i]['current_condition']['humidity']
-                row.condition_climatique = data[i]['current_condition']['condition']
+                row.Meteo = []
+                row.Meteo.Temperature = data[i]['current_condition']['tmp']
+                row.Meteo.Wind_speed = data[i]['current_condition']['wnd_spd']
+                row.Meteo.Wind_direction = data[i]['current_condition']['wnd_dir']
+                row.Meteo.pressure = data[i]['current_condition']['pressure']
+                row.Meteo.humidity = data[i]['current_condition']['humidity']
+                row.Meteo.condition_climatique = data[i]['current_condition']['condition']
             } catch (e) {}
             data2.push(row);
         }
@@ -40,6 +41,20 @@ function cleanData(data, type) {
     }
 
     if (type == 'trends') {
+        data2.Code_region = data[0][0]['Code']
+        data2.Region = data[0][0]['Libelle']
+        data2.Date = data[0][0]['Date']
+        data2.Hour = data[0][0]['Hour']
+        data2.Taux_chomage = data[0][0]['Taux_chomage']
+        data2.Meteo = []
+        data2.Meteo.Temperature = data[0][0]['Temperature']
+        data2.Meteo.Wind_speed = data[0][0]['Wind_speed']
+        data2.Meteo.Wind_direction = data[0][0]['Wind_direction']
+        data2.Meteo.pressure = data[0][0]['pressure']
+        data2.Meteo.humidity = data[0][0]['humidity']
+        data2.Meteo.condition_climatique = data[0][0]['condition_climatique']
+        data2.Popularite = data[0][0]['value'][0]
+        data2.Films = []
         for (let i = 0; i < data.length; i++) {
             let row = {}
             row.Id = data[i]['id']
@@ -51,20 +66,9 @@ function cleanData(data, type) {
             row.Length = data[i]['length']
             row.Genres = data[i]['genres']
             row.Original_language = data[i]['language']
-            row.Code_region = data[i][0]['Code']
-            row.Region = data[i][0]['Libelle']
-            row.Date = data[i][0]['Date']
-            row.Hour = data[i][0]['Hour']
-            row.Taux_chomage = data[i][0]['Taux_chomage']
-            row.Temperature = data[i][0]['Temperature']
-            row.Wind_speed = data[i][0]['Wind_speed']
-            row.Wind_direction = data[i][0]['Wind_direction']
-            row.pressure = data[i][0]['pressure']
-            row.humidity = data[i][0]['humidity']
-            row.condition_climatique = data[i][0]['condition_climatique']
-            row.Popularite = data[i][0]['value'][0]
-            data2.push(row);
+            data2.Films.push(row);
         }
+        console.log(data2)
     }
 
     return data2
