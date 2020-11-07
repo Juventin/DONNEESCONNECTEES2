7d497fc8b5e2f7afb0cb4a8312e902c2a105f772
Juventin <53041536+Juventin@users.noreply.github.com>
2020-11-07 10:03:08 +0100

    cleaning + routes XML

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { cleanData } from './cleanData.js';
+import { cleanData, mergeData, mergeDataNoJointure } from './functions.js';
 import { createRequire } from 'module';
 import pkg from 'jstoxml';
 const { toXML } = pkg;
@@ -20,11 +20,9 @@ var path = require('path');
 const googleTrends = require('google-trends-api');
 
 var cors = require('cors');
-var corsOrigins = [
-    // 'https://juventin.github.io/DONNEESCONNECTEES2/', // PROD
-    '*' // DEV
-]
+var corsOrigins = '*'
 
+app.use(cors({origin: corsOrigins}));
 
 
 app.get("/", function(req, res){
@@ -42,33 +40,6 @@ function getRegions() {
     return data
 }
 
-function mergeData(arr1, arr1key, arr2, arr2key) {
-
-    let merged = [];
-
-    for (let i = 0; i < arr1.length; i++) {
-        merged.push({
-            ...arr1[i],
-                ...(arr2.find((truc) => truc[arr2key] === arr1[i][arr1key]))
-        });
-    }
-
-    return merged;
-}
-
-function mergeDataNoJointure(arr1, arr2) {
-
-    let merged = [];
-    for (let i = 0; i < arr1.length; i++) {
-        merged.push({
-            ...arr1[i],
-                ...arr2
-        });
-    }
-
-    return merged;
-}
-
 app.get("/vocabulary", function(req, res){
     res.setHeader('Access-Control-Allow-Origin', corsOrigins);
 
@@ -76,7 +47,7 @@ app.get("/vocabulary", function(req, res){
 
     let xml = fs.readFileSync(filePath);
 
-    res.setHeader('Content-disposition', 'attachment; filename=rdfvocabulary.xml'); //do nothing
+    res.setHeader('Content-disposition', 'attachment; filename=rdfvocabulary.xml');
     res.set('Content-Type', 'application/xml');
     res.send(xml);
 })
@@ -104,15 +75,12 @@ app.get("/trends/:movie/:region", async function (req, res) {
             films = json;
         })
 
-
     // On récupère le film demandé
     var region = decodeURI(req.params.region.toLowerCase());
     function checkRegion(regioncheck) {
         return regioncheck.Libellelow == region;
     }
     var indexRegion = regions.findIndex(checkRegion);
-    console.log(indexRegion);
-
 
     // On récupère les données météo
     var result = '';
@@ -137,43 +105,124 @@ app.get("/trends/:movie/:region", async function (req, res) {
     // On récupère les données google trends
     // Et on fait notre fusion de données avec ces données
     googleTrends.interestByRegion({
-            keyword: movie,
-            geo: "FR",
-            resolution: "REGION",
-            startTime: yesterday
+        keyword: movie,
+        geo: "FR",
+        resolution: "REGION",
+        startTime: yesterday
+    })
+    .then(res => JSON.parse(res))
+    .then(json => {
+        
+        var trends = json['default']['geoMapData'];
+
+        // On jointe les deux merged sur code == Code
+        var merged = mergeData(meteo_arr, "code", regions, "Code");
+        merged = cleanData(merged, 'region')
+        
+        // On jointe les deux sur Libelle == geoName
+        var merged2 = mergeData(merged, 'Region', trends, 'geoName');
+        // On joint merged avec film
+        var merged3 = mergeDataNoJointure(films.movies, merged2);
+        merged3 = cleanData(merged3, 'trends', movie)
+
+        // On le renvoie
+        res.format({
+            'application/json': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=trends.json');
+                res.set('Content-Type', 'application/json');
+                res.json(merged3);
+            },
+            'application/rdf+xml': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=trends.xml');
+                res.set('Content-Type', 'application/xml');
+                res.send(toXML(merged3));
+            }
         })
-        .then(res => JSON.parse(res))
+    })
+})
+
+
+app.get("/trendsXML/:movie/:region", async function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', corsOrigins);
+
+    // On récupère régions
+    var regions = getRegions();
+
+    // On récupère le film demandé
+    var movie = decodeURI(req.params.movie);
+    console.log(movie)
+
+    // On récupère les données du film demandé
+    var films;
+    var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
+    await fetch(url, {
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        films = json;
+    })
+
+    // On récupère le film demandé
+    var region = decodeURI(req.params.region.toLowerCase());
+    function checkRegion(regioncheck) {
+        return regioncheck.Libellelow == region;
+    }
+    var indexRegion = regions.findIndex(checkRegion);
+
+    // On récupère les données météo
+    var result = '';
+    var meteo_arr = new Array();
+    var lat = regions[indexRegion].lat;
+    var lng = regions[indexRegion].lng;
+    var key = regions[indexRegion].Code;
+    await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
         .then(json => {
-            //console.log(result);
-
-            // Ok pour trends
-            var trends = json['default']['geoMapData'];
-
-            // On jointe les deux merged sur code == Code
-            var merged = mergeData(meteo_arr, "code", regions, "Code");
-            merged = cleanData(merged, 'region')
-            
-            // On jointe les deux sur Libelle == geoName
-            var merged2 = mergeData(merged, 'Region', trends, 'geoName');
-            // On joint merged avec film
-            var merged3 = mergeDataNoJointure(films.movies, merged2);
-            merged3 = cleanData(merged3, 'trends', movie)
-
-            // On le renvoie
-            res.format({
-                'application/json': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-                    res.set('Content-Type', 'application/json');
-                    res.json(merged3);
-                },
-                'application/rdf+xml': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
-                    res.set('Content-Type', 'application/xml');
-                    res.send(toXML(merged3));
-                }
-            })
+            var meteo = json;
+            meteo.code = key;
+            meteo_arr.push(meteo);
         })
 
+    var yesterday = new Date(new Date().setDate(new Date().getDate() - 10))
+
+    // On récupère les données google trends
+    // Et on fait notre fusion de données avec ces données
+    googleTrends.interestByRegion({
+        keyword: movie,
+        geo: "FR",
+        resolution: "REGION",
+        startTime: yesterday
+    })
+    .then(res => JSON.parse(res))
+    .then(json => {
+        //console.log(result);
+
+        // Ok pour trends
+        var trends = json['default']['geoMapData'];
+
+        // On jointe les deux merged sur code == Code
+        var merged = mergeData(meteo_arr, "code", regions, "Code");
+        merged = cleanData(merged, 'region')
+        
+        // On jointe les deux sur Libelle == geoName
+        var merged2 = mergeData(merged, 'Region', trends, 'geoName');
+
+        // On joint merged avec film
+        var merged3 = mergeDataNoJointure(films.movies, merged2);
+        merged3 = cleanData(merged3, 'trends', movie)
+
+        // On le renvoie
+        res.setHeader('Content-disposition', 'attachment; filename=trends.xml');
+        res.set('Content-Type', 'application/xml');
+        res.send(toXML(merged3));
+    })
 })
 
 
@@ -187,33 +236,56 @@ app.get("/movie/:movie", async function (req, res) {
 
 
     await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json'
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        films = json;
+        films = cleanData(films['movies'], 'films', movie)
+        console.log(films)
+
+        res.format({
+            'application/json': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=movie.json');
+                res.set('Content-Type', 'application/json');
+                res.json(films);
+            },
+            'application/rdf+xml': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=movie.xml');
+                res.set('Content-Type', 'application/xml');
+                res.send(toXML(films));
             }
         })
-        .then(res => res.json())
-        .then(json => {
-            films = json;
-            films = cleanData(films['movies'], 'films', movie)
-            console.log(films)
-
-            res.format({
-                'application/json': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-                    res.set('Content-Type', 'application/json');
-                    res.json(films);
-                },
-                'application/rdf+xml': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
-                    res.set('Content-Type', 'application/xml');
-                    res.send(toXML(films));
-                }
-            })
-        })
-
-
-
+    })
 })
+
+app.get("/movieXML/:movie", async function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', corsOrigins);
+
+    var movie = decodeURI(req.params.movie);
+    // On récupère les données du film demandé
+    var films;
+    var url = "https://api.betaseries.com/movies/search?key=c3796994ef78&title=" + movie;
+
+    await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    .then(res => res.json())
+    .then(json => {
+        films = json;
+        films = cleanData(films['movies'], 'films', movie)
+        
+        res.setHeader('Content-disposition', 'attachment; filename=movie.xml');
+        res.set('Content-Type', 'application/xml');
+        res.send(toXML(films));
+    })
+})
+
 
 app.get("/region/:region", async function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', corsOrigins);
@@ -228,8 +300,61 @@ app.get("/region/:region", async function (req, res) {
         return regioncheck.Libellelow == region;
     }
     var indexRegion = regions.findIndex(checkRegion);
-    console.log(indexRegion);
 
+    // On récupère les données météo
+    var result = '';
+    var meteo = new Array();
+    var lat = regions[indexRegion].lat;
+    var lng = regions[indexRegion].lng;
+    var key = regions[indexRegion].Code;
+    await fetch('https://www.prevision-meteo.ch/services/json/lat=' + lat + 'lng=' + lng, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        var meteo = json;
+        var meteo_arr = new Array();
+        meteo.code = key;
+        meteo_arr.push(meteo);
+
+        // On jointe les deux merged sur code == Code
+        var merged = mergeData(meteo_arr, "code",regions, "Code");
+        merged = cleanData(merged, 'region')
+
+        // On le renvoie
+        res.format({
+            'application/json': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=region.json');
+                res.set('Content-Type', 'application/json');
+                res.json(merged);
+            },
+            'application/rdf+xml': function () {
+                res.setHeader('Content-disposition', 'attachment; filename=region.xml');
+                res.set('Content-Type', 'application/xml');
+                res.send(toXML(merged));
+            }
+        })
+
+    })
+})
+
+
+app.get("/regionXML/:region", async function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', corsOrigins);
+
+    // On récupère régions
+    var regions = getRegions();
+
+    // On récupère le film demandé
+    var region = decodeURI(req.params.region.toLowerCase());
+
+    function checkRegion(regioncheck) {
+        return regioncheck.Libellelow == region;
+    }
+    var indexRegion = regions.findIndex(checkRegion);
+    console.log(indexRegion);
 
     // On récupère les données météo
     var result = '';
@@ -249,29 +374,18 @@ app.get("/region/:region", async function (req, res) {
             meteo.code = key;
             meteo_arr.push(meteo);
 
-
             // On jointe les deux merged sur code == Code
             var merged = mergeData(meteo_arr, "code",regions, "Code");
             merged = cleanData(merged, 'region')
 
             // On le renvoie
-            res.format({
-                'application/rdf+xml': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.xml'); //do nothing
-                    res.set('Content-Type', 'application/xml');
-                    res.send(toXML(merged));
-                },
-                'application/json': function () {
-                    res.setHeader('Content-disposition', 'attachment; filename=score.json'); //do nothing
-                    res.set('Content-Type', 'application/json');
-                    res.json(merged);
-                }
-            })
+            res.setHeader('Content-disposition', 'attachment; filename=region.xml');
+            res.set('Content-Type', 'application/xml');
+            res.send(toXML(merged));
 
         })
 })
 
-app.use(cors({origin: corsOrigins}));
 
 
 app.listen(port, function () {
